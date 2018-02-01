da214da1c58cb910848f609a74c1f6d54c3c43d4
Stephen Kelly <stephen_ticknevin@hotmail.com>
2018-02-01 10:45:12 +0000

    First commit
    First commit
    
    Co-Authored-By: stekell <stekell@users.noreply.github.com>

---
+++ GlitchGarden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destory on load: " + name);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+
+        Debug.Log("Play clip: " + thisLevelMusic);
+
+        if (thisLevelMusic) //if there's music attached
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
